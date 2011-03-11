01eb5d372ec43d14694d9fdff025803c0fb18916
Jason Lotito <jasonlotito@Jasons-MacBook-Pro.local>
2011-03-11 07:32:02 -0500

    Initial commit

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html>
+<head>
+<title>privacy.txt - Making privacy easier on the web</title>
+<style>
+body {
+	font-family: Courier;
+}
+</style>
+</head>
+<body>
+<header>
+	<h1>privacy.txt</h1>
+</header>
+</body>
+</html>
